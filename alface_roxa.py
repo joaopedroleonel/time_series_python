1dd23feca228792e5c4b142d127f8ee7db61faf2
João Pedro <55640904+joaopedroleonel@users.noreply.github.com>
2025-08-21 19:49:08 -0300

    add: cabecalho

---
+++ alface_roxa.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Created By: name_of_the_creator 
+# Created Date: date/month/time
+# version ='1.0'
+""" Details about the module and for what purpose it was built for""" 
+# Imports
